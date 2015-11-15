637ce7a06894eb5b3751f8d1bf06dbaecaf5a95e
Francisco Marques <kiko96kiko@gmail.com>
2015-11-15 22:18:03 +0000

    Updated Website

--- web/js/main.js
+++ web/js/main.js
@@ -13,20 +13,23 @@ $.getJSON('blockList.json', function(jsonData) {
 							"<td>" + data.reason + "</td>";
 		// isps collums
 		for(var eachIsp in data.isp){
-			if(typeof data.isp[eachIsp].status === 'undefined'){
-				newTr.innerHTML += "<td>n/a</td>";
-			}
-			else{
-				newTr.innerHTML += "<td><img alt='status' src='web/media/" + data.isp[eachIsp].status  + ".png'><span class='status'>" + data.isp[eachIsp].status + "</spam></td>";
-			}
-			newTd = document.createElement('td');
-			newTd.innerHTML = data.isp[eachIsp].dnsResponse[0];
-			newTd.setAttribute('data-blocked', data.isp[eachIsp].blocked);
-			newTr.appendChild(newTd);
-		}
+			if (eachIsp != "cabovisao") { //DISABLE CABOVISAO
+				if(typeof data.isp[eachIsp].status === 'undefined'){
+					newTr.innerHTML += "<td>n/a</td>";
+				}
+				else{
+					newTr.innerHTML += "<td><img alt='status' src='web/media/" + data.isp[eachIsp].status  + ".png'><span class='status'>" + data.isp[eachIsp].status + "</spam></td>";
+				}
+				newTd = document.createElement('td');
+				newTd.innerHTML = data.isp[eachIsp].dnsResponse[0];
+				newTd.setAttribute('data-blocked', data.isp[eachIsp].blocked);
+				newTr.appendChild(newTd);
+			};
+		};
 		// append the row		
 		document.getElementById('tableBody').appendChild(newTr);
 		domainCount++;
+		
 	});
 	document.getElementById('domainCount').innerHTML = domainCount;
 	$('#mainTable').tablesorter();
